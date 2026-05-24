bcad7a894ab5bff50f52ed94926f6572c7d946dc
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-05-24 07:46:29 -0400

    feat: :zap: Wrapper visual que reutiliza PositionComparisonPanel.

---
+++ frontend/src/components/HardSupportComparisonPanel.tsx
@@ -0,0 +1,8 @@
+import {
+  PositionComparisonPanel,
+  type PositionComparisonPanelProps,
+} from './CarryComparisonPanel'
+
+export function HardSupportComparisonPanel(props: PositionComparisonPanelProps) {
+  return <PositionComparisonPanel {...props} />
+}

df64580135c8514abc0354afed58904347369c88
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-05-24 00:36:19 -0400

    feat: :zap: Barrel de helpers compartidos: métricas, progresión, timings y tipos base.

---
+++ backend/src/services/positionBase.service.ts
@@ -0,0 +1,15 @@
+export { computeRoleMetrics, detectPosition, pickBenchmarkValue, round } from './carryMetrics.service'
+export {
+  buildNeutralItems,
+  buildPurchaseTrail,
+  buildSkillProgression,
+  buildTalentChoices,
+  compareItemTimings,
+} from './carryProgression.service'
+export type {
+  BenchmarkPercentile,
+  CarryComparisonMetric,
+  ComparisonPosition,
+  ResolvedAbilityConstant,
+  ResolvedItemConstant,
+} from './carryComparison.types'
